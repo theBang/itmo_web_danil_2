6bbf068c2e649a549f19c211613bc0adf32b79ac
Danil D <d.danil.s.96@gmail.com>
2019-09-30 10:58:56 +0300

    Some changes

--- public09092019/encrypt.js
+++ public09092019/encrypt.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 let key = fs.readFileSync('key');
 let msg = fs.readFileSync('enc');
 
-let encr = crypto.publicEncrypt({ key: key, padding: crypto.constants.RSA_PKCS1_PADDING }, msg);
-let decr = crypto.publicDecrypt(key, encr)
+let encr = crypto.publicEncrypt(key, msg);
+//let decr = crypto.publicDecrypt(key, encr)
 
 console.log(msg.toString());
 console.log(encr.toString());
 
 fs.writeFile('dansecret', encr.toString(), err => console.log(err));
-console.log(decr.toString());
+//console.log(decr.toString());
