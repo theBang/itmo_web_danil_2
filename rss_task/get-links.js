6dc94b7e2573c4b425488ee11dcd5236606a7fd8
Danil D <d.danil.s.96@gmail.com>
2019-10-08 22:28:28 +0300

    Add POST with param to fetch RSS feed

--- rss_task/get-links.js
+++ rss_task/get-links.js
@@ -1,9 +1,9 @@
 const request = require('request-promise');
 const { parseStringPromise: parse } = require('xml2js');
 
-module.exports = async () => {
+module.exports = async (n) => {
   const XML = await request('https://nodejs.org/en/feed/blog.xml');
-  const data = await parse(XML);
-
-  return data.rss.channel[0].item.map(item => new Object({ title: item.title[0], link: item.link[0] }));
+  const data = (await parse(XML)).rss.channel[0].item;
+  if (data.length < n) n = data.length;
+  return data.slice(0, n).map(item => new Object({ title: item.title[0], link: item.link[0] }));
 };
--- rss_task/index.js
+++ rss_task/index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const { promises: { readFile } } = require('fs');
+
 const getLinks = require('./get-links');
 
 const PORT = 3000;
 const app = express();
 
-app.use(cors());
 
-app.get('/', cors(), async (req, res) => {
-  res.send(await getLinks());
-});
-
-app.get('/f', async (req, res) => {
-  res.send('<html><head></head><body><script></script></body></html>');
-});
-
-app.listen(PORT, () => console.log(process.pid));
+app
+  .use(express.json())
+  .get('/', async (req, res) => res.redirect('/rss'))
+  .get('/links', cors(), async (req, res) => {
+    res.json(await getLinks(req.body.n));
+  })
+  .post('/links', cors(), async (req, res) => {
+    res.json(await getLinks(req.body.n));
+  })
+  .get('/rss', async (req, res) => 
+    res.send(await readFile('./index.html', { encoding: 'utf-8' })))
+  .listen(PORT, () => console.log(process.pid));
